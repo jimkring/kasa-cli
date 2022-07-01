38d3814039769ce6c4460fd2af362042e696a8e6
Jim Kring <jim.kring@jki.net>
2022-07-01 12:20:53 -0700

    changed to super-simple build

--- kasa_cli/__main__.py
+++ kasa_cli/__main__.py
@@ -1,11 +1,12 @@
 __version__ = '0.1.0'
-import asyncio
-from kasa import cli
+# import asyncio
+# from kasa import cli
 
 if __name__ == "__main__":
     
     # fixes a "raise RuntimeError('Event loop is closed')" error
     # https://stackoverflow.com/questions/45600579/asyncio-event-loop-is-closed-when-getting-loop
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    # asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     
-    cli.cli()
+    # cli.cli()
+    print("hello world!")
